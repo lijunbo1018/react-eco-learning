f51362b2c11738a3b7806f41719ff70de7744b6e
lijunbo1018 <lijunbo1018@gmail.com>
2017-04-11 21:55:05 +0800

    CSSTransitionGroup demo

--- src/index/route.js
+++ src/index/route.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react'
-import { NavItem, PureNavItem } from './pureComponent'
+import TodoList from '../transition/cssTransition'
 import style from './index.less'
 
 class Container extends Component {
     constructor(props) {
-        super(props);
-        this.onClick = this.onClick.bind(this);
-        this.state = {
-            className: ''
-        }
-    }
-    onClick() {
-        const { className } = this.state;
-        this.setState({ className: className ? '' : style.highlight })
+        super(props)
     }
     render() {
         return (
-            <div className={this.state.className}>
-                <NavItem text="non-pure nav item" />
-                <PureNavItem text="pure nav item" />
-                <PureNavItem text="broken pure nav item" env={+ new Date()} />
-                <button onClick={this.onClick}>Click to toggle</button>
-            </div>
+            <TodoList />
         )
     }
 }
