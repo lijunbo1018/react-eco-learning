fb84ed9ef9a2da8c9198107189efdce36f51e14b
lijunbo1018 <lijunbo1018@gmail.com>
2017-04-12 12:45:28 +0800

    complete CSSTransitionGroup demo

--- src/transition/cssTransition.js
+++ src/transition/cssTransition.js
@@ -30,12 +30,24 @@ export default class extends Component {
         ));
         return (
             <div>
-                <button onClick={this.handleAdd}>Add Item</button>
                 <CSSTransitionGroup
-                    transitionName="animate"
+                    transitionName={{
+                        enter: style.enter,
+                        enterActive: style.enterActive,
+                        leave: style.leave,
+                        leaveActive: style.leaveActive
+                    }}
                     transitionEnterTimeout={parseInt(style.enterDuration, 10)}
                     transitionLeaveTimeout={parseInt(style.leaveDuration, 10)}>
                     {items}
+                </CSSTransitionGroup>
+                <CSSTransitionGroup
+                    transitionName="button"
+                    transitionAppear
+                    transitionAppearTimeout={parseInt(style.enterDuration, 10)}
+                    transitionEnter={false}
+                    transitionLeave={false}>
+                    <button onClick={this.handleAdd}>Add Item</button>
                 </CSSTransitionGroup>
             </div>
         )
